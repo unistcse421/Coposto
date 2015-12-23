f33c71edb242744eea00a7459c5c6dccc5f189ad
beknazar <beknazar23@gmail.com>
2015-12-23 21:33:55 +0900

    fixed csv cities

--- head/views.py
+++ head/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render_to_response
 
 def index(request):
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # context = {'latest_question_list': latest_question_list}
-    return render_to_response('index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
