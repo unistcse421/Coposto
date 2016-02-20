a723c3eec31d349f933eec831fb72e170f1b6612
beknazar <beknazar23@gmail.com>
2016-02-20 18:34:46 +0900

    russian translation is done 90 %

--- head/forms.py
+++ head/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from head.models import Parcel, City
+from head.functions import home_context
 
 PARCEL_TYPES = [('type1', '< 1kg'), 
 				('type2', '1-3kg'),
@@ -16,14 +17,14 @@ class ParcelForm(ModelForm):
 		# fields = ['destination_a', 'destination_b', 'date_a', 'date_b', 'parcel_name', 'description', 'weight', 'price']
 		exclude = ['profile_a', 'profile_b', 'parcel_category']
 		widgets = {
-            'destination_a' : forms.TextInput(attrs={'placeholder': 'From', 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
-            'destination_b' : forms.TextInput(attrs={'placeholder': 'To', 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
-            'date_a' : forms.TextInput(attrs={'placeholder': 'From', 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
-            'date_b' : forms.TextInput(attrs={'placeholder': 'To', 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
-            'parcel_name' : forms.TextInput(attrs={'placeholder': 'Parcel name', 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
-            'description' : forms.Textarea(attrs={'placeholder': 'Description', 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
-            'weight' : forms.TextInput(attrs={'placeholder': 'Parcel weight in kg', 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
-            'price' : forms.TextInput(attrs={'placeholder': 'How much would like to pay?', 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'})
+            'destination_a' : forms.TextInput(attrs={'placeholder': home_context['from_dest'], 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
+            'destination_b' : forms.TextInput(attrs={'placeholder': home_context['to_dest'], 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
+            'date_a' : forms.TextInput(attrs={'placeholder': home_context['from_date'], 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
+            'date_b' : forms.TextInput(attrs={'placeholder': home_context['to_date'], 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
+            'parcel_name' : forms.TextInput(attrs={'placeholder': home_context['parcel_name'], 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
+            'description' : forms.Textarea(attrs={'placeholder': home_context['parcel_description'], 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
+            'weight' : forms.TextInput(attrs={'placeholder': home_context['weight'], 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'}),
+            'price' : forms.TextInput(attrs={'placeholder': home_context['price'], 'class': 'form-control', 'required': 'true', 'data-remote': '/send_form_validator/'})
         }
 	
 	image = forms.ImageField()
