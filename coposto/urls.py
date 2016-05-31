4a98ce26a04a2cc22727078faea1ce19a361566c
topcoder <topcoderkz@gmail.com>
2016-06-01 03:39:34 +0900

    delete temp

--- coposto/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url, patterns
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^', include('head.urls')),
-    url(r'^admin/', admin.site.urls),
-]
--- coposto/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-
-# from .models import Question
-
-def index(request):
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # context = {'latest_question_list': latest_question_list}
-    return render_to_response('index.html')
